5a512685f9078e8ac369fa4ea8706cfff78011b2
twin-cant-code <156682822+twin-cant-code@users.noreply.github.com>
2024-01-15 15:19:01 +0100

    chest loot fr fr

--- 11.31 Apollo/Looting.h
+++ 11.31 Apollo/Looting.h
@@ -3,117 +3,44 @@
 #include "Hooks.h"
 #include "Player.h"
 
+struct ItemRow
+{
+	UFortItemDefinition* Def;
+	int Level = -1;
+	int DropCount;
+	float Weight;
+	int LoadedAmmo = 0;
+};
+
+
 namespace looting
 {
 	// yay
 	static bool bspawnloot = true;
 }
 
-void InitLooting()
+
+if (IsFactionChest)
 {
-	void InitLooting()
+	/*for (size_t i = 0; i < 2 /*3* /; i++)
+	{*/
+	ItemRow* Item = GetRandomItem(TierGroupStr, EFortItemType::WeaponRanged);
+	while (!Item) {
+		Item = GetRandomItem(TierGroupStr, EFortItemType::WeaponRanged);
+	}
+	if (!Item || !Item->Def)
+		return 0;
+
+	int Ammo2 = 0;
+	if (Item->Def->GetName().starts_with("WID_Assault_LMG_Athena_")) {
+		Ammo2 = 45;
+	}
+
+	SpawnPickup(Loc, Item->Def, Item->DropCount, Item->LoadedAmmo, Flag, Source);
+	UFortAmmoItemDefinition* AmmoDef = (UFortAmmoItemDefinition*)((UFortWeaponRangedItemDefinition*)Item->Def)->GetAmmoWorldItemDefinition_BP();
+	if (AmmoDef && Item->Def != AmmoDef && (AmmoDef->DropCount > 0 || Ammo2 > 0) && AmmoDef->GetName() != "AmmoInfiniteCrossbow_Athena")
 	{
-		//AR
-		ItemRow Row1{};
-#ifndef TESTING_LOOTPOOL
-#ifndef ONE_SHOT
-		Row1.Def = StaticLoadObject<UFortItemDefinition>("/Game/Athena/Items/Weapons/WID_Assault_Auto_Athena_C_Ore_T02.WID_Assault_Auto_Athena_C_Ore_T02");
-		Row1.Level = 0;
-		Row1.DropCount = 1;
-		Row1.LoadedAmmo = GetAmmoForDef((UFortWeaponItemDefinition*)Row1.Def);
-		Row1.Weight = 0.15f;
-		LootForTiers["Loot_AthenaFloorLoot_Warmup"].push_back(Row1);
-		LootForTiers["Loot_AthenaFloorLoot"].push_back(Row1);
-		LootForTiers["Loot_AthenaTreasure"].push_back(Row1);
-		LootForTiers["Loot_AthenaVending"].push_back(Row1);
-
-		Row1.Def = StaticLoadObject<UFortItemDefinition>("/Game/Athena/Items/Weapons/WID_Assault_Auto_Athena_UC_Ore_T03.WID_Assault_Auto_Athena_UC_Ore_T03");
-		Row1.Level = 1;
-		Row1.DropCount = 1;
-		Row1.LoadedAmmo = GetAmmoForDef((UFortWeaponItemDefinition*)Row1.Def);
-		Row1.Weight = 0.30f;
-		LootForTiers["Loot_AthenaFloorLoot_Warmup"].push_back(Row1);
-		LootForTiers["Loot_AthenaFloorLoot"].push_back(Row1);
-		Row1.Weight = 0.15f;
-		LootForTiers["Loot_AthenaTreasure"].push_back(Row1);
-		LootForTiers["Loot_AthenaVending"].push_back(Row1);
-
-		Row1.Def = StaticLoadObject<UFortItemDefinition>("/Game/Athena/Items/Weapons/WID_Assault_Auto_Athena_R_Ore_T03.WID_Assault_Auto_Athena_R_Ore_T03");
-		Row1.Level = 2;
-		Row1.DropCount = 1;
-		Row1.LoadedAmmo = GetAmmoForDef((UFortWeaponItemDefinition*)Row1.Def);
-		Row1.Weight = 0.10f;
-		LootForTiers["Loot_AthenaFloorLoot_Warmup"].push_back(Row1);
-		LootForTiers["Loot_AthenaFloorLoot"].push_back(Row1);
-		Row1.Weight = 0.35f;
-		LootForTiers["Loot_AthenaTreasure"].push_back(Row1);
-		LootForTiers["Loot_AthenaVending"].push_back(Row1);
-
-		Row1.Def = StaticLoadObject<UFortItemDefinition>("/Game/Athena/Items/Weapons/WID_Assault_AutoHigh_Athena_VR_Ore_T03.WID_Assault_AutoHigh_Athena_VR_Ore_T03");
-		Row1.Level = 3;
-		Row1.DropCount = 1;
-		Row1.LoadedAmmo = GetAmmoForDef((UFortWeaponItemDefinition*)Row1.Def);
-		Row1.Weight = 0.05f;
-		LootForTiers["Loot_AthenaTreasure"].push_back(Row1);
-		LootForTiers["Loot_AthenaVending"].push_back(Row1);
-
-		Row1.Def = StaticLoadObject<UFortItemDefinition>("/Game/Athena/Items/Weapons/WID_Assault_AutoHigh_Athena_SR_Ore_T03.WID_Assault_AutoHigh_Athena_SR_Ore_T03");
-		Row1.Level = 4;
-		Row1.DropCount = 1;
-		Row1.LoadedAmmo = GetAmmoForDef((UFortWeaponItemDefinition*)Row1.Def);
-		Row1.Weight = 0.01f;
-		LootForTiers["Loot_AthenaTreasure"].push_back(Row1);
-		LootForTiers["Loot_AthenaVending"].push_back(Row1);
-		//End AR
-
-		//Burst Rifle
-		Row1.Def = StaticLoadObject<UFortItemDefinition>("/Game/Athena/Items/Weapons/WID_Assault_SemiAuto_Athena_C_Ore_T02.WID_Assault_SemiAuto_Athena_C_Ore_T02");
-		Row1.Level = 0;
-		Row1.DropCount = 1;
-		Row1.LoadedAmmo = GetAmmoForDef((UFortWeaponItemDefinition*)Row1.Def);
-		Row1.Weight = 0.15f;
-		LootForTiers["Loot_AthenaFloorLoot_Warmup"].push_back(Row1);
-		LootForTiers["Loot_AthenaFloorLoot"].push_back(Row1);
-		LootForTiers["Loot_AthenaTreasure"].push_back(Row1);
-		LootForTiers["Loot_AthenaVending"].push_back(Row1);
-
-		Row1.Def = StaticLoadObject<UFortItemDefinition>("/Game/Athena/Items/Weapons/WID_Assault_SemiAuto_Athena_UC_Ore_T03.WID_Assault_SemiAuto_Athena_UC_Ore_T03");
-		Row1.Level = 1;
-		Row1.DropCount = 1;
-		Row1.LoadedAmmo = GetAmmoForDef((UFortWeaponItemDefinition*)Row1.Def);
-		Row1.Weight = 0.30f;
-		LootForTiers["Loot_AthenaFloorLoot_Warmup"].push_back(Row1);
-		LootForTiers["Loot_AthenaFloorLoot"].push_back(Row1);
-		Row1.Weight = 0.15f;
-		LootForTiers["Loot_AthenaTreasure"].push_back(Row1);
-		LootForTiers["Loot_AthenaVending"].push_back(Row1);
-
-		Row1.Def = StaticLoadObject<UFortItemDefinition>("/Game/Athena/Items/Weapons/WID_Assault_SemiAuto_Athena_R_Ore_T03.WID_Assault_SemiAuto_Athena_R_Ore_T03");
-		Row1.Level = 2;
-		Row1.DropCount = 1;
-		Row1.LoadedAmmo = GetAmmoForDef((UFortWeaponItemDefinition*)Row1.Def);
-		Row1.Weight = 0.10f;
-		LootForTiers["Loot_AthenaFloorLoot_Warmup"].push_back(Row1);
-		LootForTiers["Loot_AthenaFloorLoot"].push_back(Row1);
-		Row1.Weight = 0.35f;
-		LootForTiers["Loot_AthenaTreasure"].push_back(Row1);
-		LootForTiers["Loot_AthenaVending"].push_back(Row1);
-
-		Row1.Def = StaticLoadObject<UFortItemDefinition>("/Game/Athena/Items/Weapons/WID_Assault_SemiAuto_Athena_VR_Ore_T03.WID_Assault_SemiAuto_Athena_VR_Ore_T03");
-		Row1.Level = 3;
-		Row1.DropCount = 1;
-		Row1.LoadedAmmo = GetAmmoForDef((UFortWeaponItemDefinition*)Row1.Def);
-		Row1.Weight = 0.05f;
-		LootForTiers["Loot_AthenaTreasure"].push_back(Row1);
-		LootForTiers["Loot_AthenaVending"].push_back(Row1);
-
-		Row1.Def = StaticLoadObject<UFortItemDefinition>("/Game/Athena/Items/Weapons/WID_Assault_SemiAuto_Athena_SR_Ore_T03.WID_Assault_SemiAuto_Athena_SR_Ore_T03");
-		Row1.Level = 4;
-		Row1.DropCount = 1;
-		Row1.LoadedAmmo = GetAmmoForDef((UFortWeaponItemDefinition*)Row1.Def);
-		Row1.Weight = 0.01f;
-		LootForTiers["Loot_AthenaTreasure"].push_back(Row1);
-		LootForTiers["Loot_AthenaVending"].push_back(Row1);
-		//End Burst Rifle
-
+		SpawnPickup(Loc, AmmoDef, Ammo2 == 0 ? AmmoDef->DropCount == 4 ? 6 : (AmmoDef->DropCount == 10 ? 2 : AmmoDef->DropCount) : Ammo2, 0, Flag, Source);
+	}
+	}
 }
